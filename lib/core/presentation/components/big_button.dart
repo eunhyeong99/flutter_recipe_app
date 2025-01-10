60d07179366be3f8cdc9a0eb4963b105681e1ce6
eunhyeong99 <whdmsgud8@naver.com>
2025-01-10 18:47:04 +0900

    feat: 공통 component button.

---
+++ lib/core/presentation/components/big_button.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../../ui/color_styles.dart';
+import '../../../ui/text_styles.dart';
+
+class BigButton extends StatefulWidget {
+  final String text;
+  final void Function() onPressed;
+
+  const BigButton(
+    this.text, {
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  State<BigButton> createState() => _BigButtonState();
+}
+
+class _BigButtonState extends State<BigButton> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          isPressed = false;
+        });
+        widget.onPressed();
+      },
+      onTapCancel: () {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: isPressed ? ColorStyles.gray4 : ColorStyles.primary100,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.text,
+              style: TextStyles.normalTextBold.copyWith(color: Colors.white),
+            ),
+            const SizedBox(width: 11),
+            const Icon(
+              Icons.arrow_forward,
+              size: 20,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/core/presentation/components/small_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../ui/color_styles.dart';
+import '../../../ui/text_styles.dart';
+
+class SmallButton extends StatefulWidget {
+  final String text;
+  final void Function() onPressed;
+
+  const SmallButton(
+    this.text, {
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  State<SmallButton> createState() => _SmallButtonState();
+}
+
+class _SmallButtonState extends State<SmallButton> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          isPressed = false;
+        });
+        widget.onPressed();
+      },
+      onTapCancel: () {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      child: Container(
+        height: 37,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: isPressed ? ColorStyles.gray4 : ColorStyles.primary100,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.text,
+              style: TextStyles.normalTextBold.copyWith(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
